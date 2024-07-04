c41fb68adeee4531db9d31f9c01d91751d780ad2
Ravindra <sirviravindra609@gmail.com>
2024-07-04 14:46:25 +0530

    Refactor googleSheets.ts to use environment variables for client email, private key, and spreadsheet ID

--- src/ googleSheets.ts
+++ src/ googleSheets.ts
@@ -1,8 +1,7 @@
 import { google } from "googleapis";
 
-const client_email = "apticon@macro-magpie-424101-r4.iam.gserviceaccount.com";
-const private_key =
-  "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDEnt9ebgIa2SnU\nSt5vsvuY3gzR4iTUa78exJ/mRlpj63tEmXc4XUpoP355w0AJ7o39q72tv/KVQlNl\n8RE4oO3m8LzA14P540UbC9DDJ7NfXX53gjfiBKWf3eU1hgk0iX6dDZqHo1D4TmBY\nJbe5S9jKaC8eOvq2xJZfkQfSbHtaMTgpqUnZiHNFtFCbIHGFRT6MVIjThZaa00hl\nLBLAg35NJD1sq/+emO8ZOPvXZfBpUkqc5Y8UWoapssnO8xvcLWL8Kk79K+5pzFoW\nKJBKJr9rQzrm+3B/F4TkJ+ZLgTDwr/nJuje29txNdlA0BD85gMQa4OyOOatFetZS\nRUAnjx4tAgMBAAECggEAJYac4wzj4fPog9f2NCNY2YgnsjcTCGe4BktIqU5sGf3r\nfOA9XMiRVuwnW13kbwDUoMGSZgLobYxHFoG347LzNMxlefin+51gpxTONzDwjxbs\n+ComOEDiy2wyPtDiGODVBWx/PbvxzDJJOQmZ0weizJ6tv3tKNm+2s26eq46AzGma\nR3fcidYSuiknX4QV490JnGQVWCjXDOnbMtRWHfj6LWdbMwvcpcucu1CXjhKnrQX1\nG/9OzNeHuD5Mgv3yxmg7Z2yFxetW79d51UMkbmuPafWl5LIBDUX6JCT/sopcBniv\nMo1bjc8W3M9jEs7ixIjUZlHeLSa3BRsfAUHtTi8CgQKBgQD54LaNuCs2GfSWwPGg\nLdRFbw/rPfLWTuh2IhMEL/FhaubFvJU2o1OuChQWeY+rhhNIb5xI0ZGDDgeyP5Sy\n1voTe35/yRylMBNRpidgIAPjS2tseOsuTcs0XGYn6N9dzF423YvU1ygFFXy7u7Da\naRVPmqfdupUJcSwBh/iHVN9IlQKBgQDJcB5zuYCtFEFfdOx56HNmIftO/ISarvVh\nRVFcyGxJc+7cvsM0W5+VkYg5eM7dftfPyc79JmlMo3x+cPIQL8q6grglmwbBGK5G\nJoyn56jr5Mtyhsok5hTm9aQHa61lv+9nffndsVW+qRHlo0QiWXlvcUbNMGF/SOzq\nUExCmpnhOQKBgC91JVbPNq3EyjZjmMCCU8GIMW7NSjS3seDV4eS2slJ9Oi0t/uzs\nojT4xw3psoayJTFpuTX/T3KjlISfHL/GoqJWnxTn/l/FPLYSJ4JTMknWBiYI5Snb\nLo/YSLKlwN6QtQgYlbkrz/b/OLxzDki9EoYW6nDrQekULHZx+5rAXNPtAoGAG0Qw\n6ws2DreKLcie5dWAIVgCM2vc6y85icQfQkbi2YJ216x+scyhKla7sMPYhITR4492\nlC+p2zTZ+ieBrI07jMl68aQtYt/bXyWCroueAuhCXas/G4WFWMnxPQvnObmkR0b3\nFnoVmCw8fu/9ngSselhD9CxTVi5h20mpOO7CW4kCgYBf4IgDiNU5qz+mQb7nL+tG\n2FpqRz1ez7njac3owwmGjnRqjMCorUTOoogSGSaOwGf6r0qaqM6sw1QYOyeDPaII\n9QC1/gxC947njsF9n8fGUOAC3qMLiFWKmlc/PbrZWLBUnsToxOvjh1VQxalYL+WR\n/IST4+z98qsFAiDFWecLAQ==\n-----END PRIVATE KEY-----\n";
+const client_email = process.env.CLIENT_EMAIL;
+const private_key = process.env.PRIVATE_KEY;
 
 // Configure a JWT auth client
 const auth = new google.auth.JWT(client_email, undefined, private_key, [
@@ -14,7 +13,7 @@ const sheets = google.sheets({ version: "v4", auth });
 
 export async function appendRowToSheet(rowData: any[]) {
   try {
-    const spreadsheetId = "1P85pkAO_42umqh_0ZgvapnYzD962hHs1prVb7rCGP1o";
+    const spreadsheetId = process.env.SPREADSHEET_ID;
     const range = "Sheet1!A1";
     const valueInputOption = "RAW";
 
---
+++ src/app/testurl/page.tsx
@@ -0,0 +1,190 @@
+"use client";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Head from "next/head";
+import { useState } from "react";
+import Image from "next/image";
+import RegistrationDialog from "@/components/registrationDialog";
+
+type FeeType = {
+  type: string;
+  fees: { [key: string]: string };
+  Price: { [key: string]: string };
+};
+
+const registrationFees: FeeType[] = [
+  {
+    type: "Early Bird (Upto 15th July)",
+    fees: {
+      "APTI Members": "₹ 2800",
+      students: "₹ 2500",
+      "non-Members": "₹ 3300",
+      industry: "₹ 4000",
+      companion: "₹ 1500",
+      international: "118 USD",
+    },
+    Price: {
+      "APTI Members": "₹ 3500",
+      students: "₹ 2900",
+      "non-Members": "₹ 4000",
+      industry: "₹ 4500",
+      companion: "",
+      international: "",
+    },
+  },
+];
+
+export default function Registration() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div className="bg-white">
+      <Head>
+        <title>Registration - APTICON 2024</title>
+        <meta
+          name="description"
+          content="Register for APTICON 2024 to secure your spot at this prestigious pharmacy conference held on September 27-28, 2024, in Bhubaneswar, Odisha. Discover registration fees, deadlines, and benefits of attending."
+        />
+        <meta
+          name="keywords"
+          content="APTICON 2024, Registration, Pharmacy Conference, Bhubaneswar, Odisha, Pharmacy Education, Conference Registration"
+        />
+        <meta name="author" content="APTICON Organizing Committee" />
+        <meta property="og:title" content="Registration - APTICON 2024" />
+        <meta
+          property="og:description"
+          content="Register for APTICON 2024 to secure your spot at this prestigious pharmacy conference held on September 27-28, 2024, in Bhubaneswar, Odisha."
+        />
+        <meta property="og:image" content="/banner1.jpg" />
+        <meta
+          property="og:url"
+          content="https://www.apticon2024.com/registration"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Registration - APTICON 2024" />
+        <meta
+          name="twitter:description"
+          content="Register for APTICON 2024 to secure your spot at this prestigious pharmacy conference held on September 27-28, 2024, in Bhubaneswar, Odisha."
+        />
+        <meta name="twitter:image" content="/banner1.jpg" />
+      </Head>
+
+      <div className="bg-white overflow-hidden fixed top-0 left-0 w-full z-50">
+        <Header />
+      </div>
+      <main className="mt-16">
+        <section
+          id="register-for-apticon"
+          className="container mx-auto py-20 text-black"
+        >
+          <h2 className="text-3xl md:text-4xl font-bold text-center mb-10 text-green">
+            Register to APTICON 2024
+          </h2>
+          <p className="text-center text-lg md:text-xl mb-10 max-w-3xl mx-auto leading-relaxed text-darkBrown">
+            Secure your spot at APTICON 2024 by registering today. Find out
+            about registration fees, deadlines, and the benefits of attending
+            the conference. Join us for an enriching experience that promises to
+            transform your understanding of pharmacy education and practice.
+          </p>
+          <div className="overflow-x-auto">
+            <h3 className="text-2xl md:text-3xl font-bold mb-4 text-ochre">
+              Registration Fees
+            </h3>
+            <div className="hidden md:block">
+              <div className="bg-grey text-black">
+                <div className="container mx-auto p-6">
+                  <div className="bg-white shadow-md rounded-lg overflow-hidden">
+                    <table className="min-w-full bg-white">
+                      <thead>
+                        <tr>
+                          <th className="py-2 px-4 bg-ashGrey text-black text-left text-sm font-semibold uppercase">
+                            Registration Fee
+                          </th>
+                          <th className="py-2 px-4 bg-ashGrey text-black text-left text-sm font-semibold uppercase">
+                            Price
+                          </th>
+                          <th className="py-2 px-4 bg-ashGrey text-black text-left text-sm font-semibold uppercase">
+                            Pay Now
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {registrationFees.map((fee, index) =>
+                          Object.entries(fee.fees).map(([key, value]) => (
+                            <tr key={index + key}>
+                              <td className="py-2 px-4 border-b border-grey">
+                                {fee.type} -{" "}
+                                {key.charAt(0).toUpperCase() + key.slice(1)}
+                              </td>
+                              <td className="py-2 px-4 border-b border-grey">
+                                <span className="line-through">
+                                  {fee.Price[key]}
+                                </span>
+                                <span className="text-red-500 ml-2">
+                                  {fee.fees[key]}
+                                </span>
+                              </td>
+                              <td className="py-2 px-4 border-b border-grey">
+                                <button
+                                  onClick={() => setIsModalOpen(true)}
+                                  className="bg-green text-white py-1 px-4 rounded inline-block cursor-pointer"
+                                >
+                                  Pay Now
+                                </button>
+                              </td>
+                            </tr>
+                          ))
+                        )}
+                      </tbody>
+                    </table>
+                    <p className="text-center mt-4 text-sm text-gray-600">
+                      Note: Fees include 18% GST.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="block md:hidden">
+              {registrationFees.map((fee, index) =>
+                Object.entries(fee.fees).map(([key, value]) => (
+                  <div
+                    key={index + key}
+                    className="bg-white shadow-md rounded-lg mb-4 p-4"
+                  >
+                    <h4 className="text-lg font-bold mb-2">
+                      {fee.type} - {key.charAt(0).toUpperCase() + key.slice(1)}
+                    </h4>
+                    <div className="mb-2">
+                      <span className="font-semibold">Price:</span>{" "}
+                      <span className="line-through">{fee.Price[key]}</span>
+                      <span className="text-red-500 ml-2">{fee.fees[key]}</span>
+                    </div>
+                    <button
+                      onClick={() => setIsModalOpen(true)}
+                      className="bg-green text-white py-1 px-4 rounded mt-2 inline-block text-right w-full cursor-pointer"
+                    >
+                      Pay Now
+                    </button>
+                  </div>
+                ))
+              )}
+              <p className="text-center mt-4 text-sm text-gray-600">
+                Note: Fees include 18% GST.
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      {isModalOpen && (
+        <RegistrationDialog
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
+
+      <Footer />
+    </div>
+  );
+}
