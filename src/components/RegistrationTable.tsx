c74891f789cb230f7d3aa69904a65fdb7b0593da
Ravindra <sirviravindra609@gmail.com>
2024-08-23 21:19:37 +0530

    Refactor RegistrationTable component to fix search input styling

--- src/components/RegistrationTable.tsx
+++ src/components/RegistrationTable.tsx
@@ -50,7 +50,7 @@ const RegistrationTable: React.FC<RegistrationTableProps> = ({ data }) => {
         <input
           type="text"
           placeholder="Search..."
-          className="flex-grow px-4 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="flex-grow px-4 py-2 border text-black border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
