a740696c619131d2aa2f8e07d51e37fc918afd36
Ravindra <sirviravindra609@gmail.com>
2024-09-12 18:46:35 +0530

    Refactor NationalBody page to include Joint Secretary information

--- src/app/national-body/page.tsx
+++ src/app/national-body/page.tsx
@@ -124,6 +124,53 @@ const NationalBody = () => {
     },
   ];
 
+  const jointSecretary = [
+    {
+      id: 15,
+      name: "Dr. Laxmidhar Maharana",
+      designation: "Professor",
+      college:
+        "School of Pharmaceutical Sciences, Siksha ‘O’ Anusandhan University,Bhubaneswar",
+      image: "https://aptiindia.org/images/committee/MAHARANA.png",
+      APTI_post: "Joint Sectretary (East)",
+    },
+    {
+      id: 16,
+      name: "Dr. Sanjay Surana",
+      designation: "Principal and Professor",
+      college:
+        "R.C.Patel Institute of Pharmaceutical Education and research, Shirpur, Maharashtra.",
+      image: "https://aptiindia.org/images/committee/central/sanjay.png",
+      APTI_post: "Joint Sectretary (West)",
+    },
+    {
+      id: 17,
+      name: "Dr. Sanjay J. Daharwal",
+      designation: "",
+      college:
+        "University Institute of Pharmacy, Pt. Ravishankar Shukla University, Raipur (C.G.)",
+      image: "https://aptiindia.org/images/committee/central/daharwal.png",
+      APTI_post: "Joint Sectretary (Central)",
+    },
+    {
+      id: 18,
+      name: "Dr. Shaista Omer",
+      designation: "Principal",
+      college: "MMJG College of Pharmacy, Ijari, Lakmapur, Haveri ,581110",
+      image: "https://aptiindia.org/images/committee/central/omer.png",
+      APTI_post: "Joint Sectretary (North)",
+    },
+    {
+      id: 19,
+      name: "Dr. Akash Ved",
+      designation: "Associate Dean Pharmacy, Director",
+      college:
+        "Dr A P J Abdul Kalam Technical University, Goel Institute of Pharmaceutical Sciences, Lucknow.",
+      image: "https://aptiindia.org/images/committee/central/akash.png",
+      APTI_post: "Joint Sectretary (South)",
+    },
+  ];
+
   return (
     <div className="px-4 py-8">
       <div className="text-center mb-8">
@@ -176,6 +223,26 @@ const NationalBody = () => {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
         {otherMembers.map((member) => (
+          <div
+            key={member.id}
+            className="p-4 bg-white shadow-md rounded-lg text-center"
+          >
+            <p className="text-green font-semibold">{member.APTI_post}</p>
+            <Image
+              src={member.image}
+              alt={member.name}
+              className="w-32 h-32 rounded-full mx-auto mb-4"
+              width={128}
+              height={128}
+            />
+            <h3 className="text-lg font-semibold text-darkBrown">
+              {member.name}
+            </h3>
+            <p className="text-gray-500 mb-2">{member.designation}</p>
+            <p className="text-gray-500">{member.college}</p>
+          </div>
+        ))}
+        {jointSecretary.map((member) => (
           <div
             key={member.id}
             className="p-4 bg-white shadow-md rounded-lg text-center"
